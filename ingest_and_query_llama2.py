ea1694f3fa0bac34149268fd18e77af884172317
recoveryuser <140160634+recoveryuser@users.noreply.github.com>
2023-09-02 20:22:20 +0800

    Add files via upload

--- ingest_and_query_llama2.py
+++ ingest_and_query_llama2.py
@@ -1,160 +1,91 @@
-import logging
+from langchain.document_loaders import ReadTheDocsLoader,PyPDFLoader
+from langchain.embeddings.base import Embeddings
+# from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter,CharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+from langchain.vectorstores import FAISS
+from typing import List
+import time
 import os
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+import ray
+# from embeddings import LocalHuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings,SentenceTransformerEmbeddings
+# To download the files locally for processing, here's the command line
+# wget -e robots=off --recursive --no-clobber --page-requisites --html-extension \
+# --convert-links --restrict-file-names=windows \
+# --domains docs.ray.io --no-parent https://docs.ray.io/en/master/
 
-import click
-import torch
-from langchain.docstore.document import Document
-from langchain.embeddings import HuggingFaceEmbeddings,HuggingFaceInstructEmbeddings
-from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
+from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import RetrievalQA
 
-from constants import (
-    CHROMA_SETTINGS,
-    DOCUMENT_MAP,
-    EMBEDDING_MODEL_NAME,
-    EMBEDDING_MODEL_PATH,
-    INGEST_THREADS,
-    PERSIST_DIRECTORY,
-    SOURCE_DIRECTORY,
-)
+FAISS_INDEX_PATH="faiss_index"
 
+def ingest_pdf(query):
+    loader = PyPDFLoader("docs/2.pdf")
+    documents = loader.load()
+    # Split document in chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+    docs = text_splitter.split_documents(documents=documents)
+    # Stage one: read all the docs, split them into chunks. 
+    st = time.time() 
+    print('Loading documents ...')
+    # docs = loader.load()
+    #Theoretically, we could use Ray to accelerate this, but it's fast enough as is. 
+    # chunks = text_splitter.create_documents([doc.page_content for doc in docs], metadatas=[doc.metadata for doc in docs])
+    et = time.time() - st
+    print(f'Time taken: {et} seconds.') 
+    # chunks =['weewewew','ewewe']
+    #Stage two: embed the docs. 
+    # embeddings = LocalHuggingFaceEmbeddings('multi-qa-mpnet-base-dot-v1')
+    embeddings = SentenceTransformerEmbeddings(model_name="/Users/sindhu/prakash/embedding_models/all-MiniLM-L6-v2")
+    print(f'Loading chunks into vector store ...') 
+    st = time.time()
+    # print(docs)
+    db = FAISS.from_documents(docs, embeddings)
+    db.save_local(FAISS_INDEX_PATH)
+    et = time.time() - st
+    print(f'Time taken: {et} seconds.')
 
-def load_single_document(file_path: str) -> Document:
-    # Loads a single document from a file path
-    file_extension = os.path.splitext(file_path)[1]
-    loader_class = DOCUMENT_MAP.get(file_extension)
-    if loader_class:
-        loader = loader_class(file_path)
-    else:
-        raise ValueError("Document type is undefined")
-    return loader.load()[0]
+def query_pdf(query):
+    # loader = ReadTheDocsLoader("docs.ray.io/en/master/")
 
-
-def load_document_batch(filepaths):
-    logging.info("Loading document batch")
-    # create a thread pool
-    with ThreadPoolExecutor(len(filepaths)) as exe:
-        # load files
-        futures = [exe.submit(load_single_document, name) for name in filepaths]
-        # collect data
-        data_list = [future.result() for future in futures]
-        # return data and file paths
-        return (data_list, filepaths)
-
-
-def load_documents(source_dir: str) -> list[Document]:
-    # Loads all documents from the source documents directory, including nested folders
-    paths = []
-    for root, _, files in os.walk(source_dir):
-        for file_name in files:
-            file_extension = os.path.splitext(file_name)[1]
-            source_file_path = os.path.join(root, file_name)
-            if file_extension in DOCUMENT_MAP.keys():
-                paths.append(source_file_path)
-
-    # Have at least one worker and at most INGEST_THREADS workers
-    n_workers = min(INGEST_THREADS, max(len(paths), 1))
-    chunksize = round(len(paths) / n_workers)
-    docs = []
-    with ProcessPoolExecutor(n_workers) as executor:
-        futures = []
-        # split the load operations into chunks
-        for i in range(0, len(paths), chunksize):
-            # select a chunk of filenames
-            filepaths = paths[i : (i + chunksize)]
-            # submit the task
-            future = executor.submit(load_document_batch, filepaths)
-            futures.append(future)
-        # process all results
-        for future in as_completed(futures):
-            # open the file and load the data
-            contents, _ = future.result()
-            docs.extend(contents)
-
-    return docs
-
-
-def split_documents(documents: list[Document]) -> tuple[list[Document], list[Document]]:
-    # Splits documents for correct Text Splitter
-    text_docs, python_docs = [], []
-    for doc in documents:
-        file_extension = os.path.splitext(doc.metadata["source"])[1]
-        if file_extension == ".py":
-            python_docs.append(doc)
-        else:
-            text_docs.append(doc)
-
-    return text_docs, python_docs
-
-
-@click.command()
-@click.option(
-    "--device_type",
-    default="cuda" if torch.cuda.is_available() else "cpu",
-    type=click.Choice(
-        [
-            "cpu",
-            "cuda",
-            "ipu",
-            "xpu",
-            "mkldnn",
-            "opengl",
-            "opencl",
-            "ideep",
-            "hip",
-            "ve",
-            "fpga",
-            "ort",
-            "xla",
-            "lazy",
-            "vulkan",
-            "mps",
-            "meta",
-            "hpu",
-            "mtia",
-        ],
-    ),
-    help="Device to run on. (Default is cuda)",
-)
-def main(device_type):
-    # Load documents and split in chunks
-    logging.info(f"Loading documents from {SOURCE_DIRECTORY}")
-    documents = load_documents(SOURCE_DIRECTORY)
-    text_documents, python_documents = split_documents(documents)
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    python_splitter = RecursiveCharacterTextSplitter.from_language(
-        language=Language.PYTHON, chunk_size=880, chunk_overlap=200
-    )
-    texts = text_splitter.split_documents(text_documents)
-    texts.extend(python_splitter.split_documents(python_documents))
-    logging.info(f"Loaded {len(documents)} documents from {SOURCE_DIRECTORY}")
-    logging.info(f"Split into {len(texts)} chunks of text")
-
-    # Create embeddings
-    # embeddings = HuggingFaceInstructEmbeddings(
-    #     model_name=EMBEDDING_MODEL_NAME,
-    #     model_kwargs={"device": device_type},
+    # text_splitter = RecursiveCharacterTextSplitter(
+    #     # Set a really small chunk size, just to show.
+    #     chunk_size = 300,
+    #     chunk_overlap  = 20,
+    #     length_function = len,
     # )
-    # change the embedding type here if you are running into issues.
-    # These are much smaller embeddings and will work for most appications
-    # If you use HuggingFaceEmbeddings, make sure to also use the same in the
-    # run_localGPT.py file.
 
-    embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_PATH, device_type="cpu")
+    n_gpu_layers = 1  # Metal set to 1 is enough.
+    n_batch = 512  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+    embeddings = SentenceTransformerEmbeddings(model_name="/Users/sindhu/prakash/embedding_models/all-MiniLM-L6-v2")
+     # Load from local storage
+    persisted_vectorstore = FAISS.load_local("faiss_index", embeddings)
 
-    db = Chroma.from_documents(
-        texts,
-        embeddings,
-        persist_directory=PERSIST_DIRECTORY,
-        client_settings=CHROMA_SETTINGS,
-
+    # Make sure the model path is correct for your system!
+    llm_llama2 = LlamaCpp(
+        model_path="/Users/sindhu/prakash/LLM-HF-models/llama-2-7b-chat.ggmlv3.q8_0/llama-2-7b-chat.ggmlv3.q8_0.bin",
+        # n_gpu_layers=n_gpu_layers,
+        n_batch=n_batch,
+        n_ctx=8192,
+        f16_kv=True,  # MUST set to True, otherwise you will run into problem after a couple of calls
+        callback_manager=callback_manager,
+        verbose=False,
     )
-   
+        # Use RetrievalQA chain for orchestration
+    qa = RetrievalQA.from_chain_type(llm=llm_llama2, chain_type="stuff", retriever=persisted_vectorstore.as_retriever())
+    result = qa.run(query)
+    print(result)
+
+def main():
+    query = input("Type in your query: \n")
+    while query != "exit":
+        query_pdf(query)
+        query = input("Type in your query: \n")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(message)s", level=logging.INFO
-    )
     main()
